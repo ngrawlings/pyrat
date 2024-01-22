00284b22c8f89c71130e073437ef39b10d8649ac
Nyhl <ngrawlings@gmai.com>
2024-01-22 18:40:05 +0100

    Updated eth_config_monitor.py to moniitor the state of wlan0

--- eth_config_monitor.py
+++ eth_config_monitor.py
@@ -1,5 +1,7 @@
 import subprocess
 import time
+import subprocess
+import time
 
 def check_eth1_ip():
     result = subprocess.run(['ip', 'addr', 'show', 'dev', 'eth1'], capture_output=True, text=True)
@@ -17,9 +19,28 @@ def check_eth1_ip():
 def set_eth1_ip(ip_address):
     subprocess.run(['ip', 'addr', 'add', f'{ip_address}/24', 'dev', 'eth1'])
 
+def check_wlan0_connection():
+    result = subprocess.run(['iwconfig', 'wlan0'], capture_output=True, text=True)
+    output = result.stdout
+
+    # Parse the output to check the connection status
+    if 'Not-Associated' in output:
+        return False
+    else:
+        return True
+    
+def configure_wifi():
+    subprocess.run(['iwlist', 'scan', 'wlan0'])
+    time.sleep(10)
+    subprocess.run(['kill', 'wpa_supplicant'])
+    subprocess.run(['wpa_supplicant', '-B', '-i', 'wlan0', '-c', '/etc/wpa_supplicant/wpa_supplicant.conf'])
+
 while True:
     current_ip = check_eth1_ip()
-    if current_ip is None or current_ip != '192.168.1.100':
-        set_eth1_ip('192.168.1.100')
-        
+    if current_ip is None or current_ip != '10.0.1.1':
+        set_eth1_ip('10.0.1.1')
+
+    if not check_wlan0_connection():
+        configure_wifi()
+
     time.sleep(60)  # Pause for 60 seconds (1 minute)
